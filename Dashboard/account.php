765e1480e4e57f6dc4ebd669bbdbcd2165686e9e
MACBOOK <alexjace151@gmail.com>
2022-04-23 04:34:41 +0100

    updated

--- Dashboard/account.php
+++ Dashboard/account.php
@@ -279,6 +279,10 @@
 		}
 		while (number < 5);
 	</script>
+
+	<script>
+		swal("Good job!", "You clicked the button!", "success");
+	</script>
 	<script src="./assets/libs/jquery/dist/jquery.min.js"></script>
 	<script src="./assets/libs/popper.js/dist/umd/popper.min.js"></script>
 	<script src="./assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
