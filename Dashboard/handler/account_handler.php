82b12ea037d3966e1470bc420440526e4cab2627
MACBOOK <alexjace151@gmail.com>
2022-04-23 16:41:01 +0100

    updated

--- Dashboard/handler/account_handler.php
+++ Dashboard/handler/account_handler.php
@@ -15,7 +15,7 @@ if(isset($_POST['update'])) {
     $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
     $file = $_FILES['image'];
 
-    if($file) {
+    if(count($file) > 0) {
         $upload_res = $uploads->uploadFile($file, "./pic/");
     
         extract($POST);
@@ -69,7 +69,7 @@ if(isset($_POST['update'])) {
     else {
         extract($POST);
         $hashed = md5($password);
-        
+
         $query = "UPDATE `users` SET `firstname` = '$firstname', `lastname` = '$lastname', `email` = '$email', `password` = '$hashed' WHERE `user_id` = '$USER_ID'";
         $result = $connect->prepare($query);
         $result->execute();
