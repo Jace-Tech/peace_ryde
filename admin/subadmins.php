19dd7b08490f9b36aea9602b91bfe7204756da02
MACBOOK <alexjace151@gmail.com>
2022-04-26 04:42:39 +0100

    updated

--- admin/subadmins.php
+++ admin/subadmins.php
@@ -30,12 +30,13 @@
                                 </div>
                             </div>
 
-                            <div x-data="{ modalOpen: false }"><button class="btn bg-indigo-500 hover:bg-indigo-600 text-white" @click.prevent="modalOpen = true" aria-controls="feedback-modal">
-                                <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
-                                    <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-                                </svg> 
-                                <span class="hidden xs:block ml-2">Add Admin</span>
-                            </button>
+                            <div x-data="{ modalOpen: false }">
+                                <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white" @click.prevent="modalOpen = true" aria-controls="feedback-modal">
+                                    <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                                        <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                                    </svg> 
+                                    <span class="hidden xs:block ml-2">Add Admin</span>
+                                </button>
                                 <div class="fixed inset-0 bg-gray-900 bg-opacity-30 z-50 transition-opacity" x-show="modalOpen" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-out duration-100" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" aria-hidden="true" x-cloak></div>
                                 <div id="feedback-modal" class="fixed inset-0 z-50 overflow-hidden flex items-center my-4 justify-center transform px-4 sm:px-6" role="dialog" aria-modal="true" x-show="modalOpen" x-transition:enter="transition ease-in-out duration-200" x-transition:enter-start="opacity-0 translate-y-4" x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in-out duration-200" x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4" x-cloak>
                                     <form method="post" action="./handler/subadmin_handler.php" class="bg-white rounded shadow-lg overflow-auto max-w-lg w-full max-h-full" @click.outside="modalOpen = false" @keydown.escape.window="modalOpen = false">
@@ -208,7 +209,105 @@
                                                             <form action="./handler/subadmin_handler.php" method="post">
                                                                 <button name="deleteAdmin" value="<?= $admin_id; ?>" class="btn text-yellow-600 text-xs bg-yellow-100 btn-sm">Suspend</button>
                                                             </form>
-                                                            <a href="<?= $admin_id; ?>" class="btn bg-blue-100 btn-sm text-xs text-blue-600 ml-2">Edit</a>
+                                                            <div x-data="{ modalOpen: false }">
+                                                                <button href="" class="btn bg-blue-100 btn-sm text-xs text-blue-600 ml-2" @click.prevent="modalOpen = true" aria-controls="feedback-modal-<?= $admin_id; ?>">Edit</button>
+                                                                <div class="fixed inset-0 bg-gray-900 bg-opacity-30 z-50 transition-opacity" x-show="modalOpen" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-out duration-100" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" aria-hidden="true" x-cloak></div>
+                                                                <div id="feedback-modal-<?= $admin_id; ?>" class="fixed inset-0 z-50 overflow-hidden flex items-center my-4 justify-center transform px-4 sm:px-6" role="dialog" aria-modal="true" x-show="modalOpen" x-transition:enter="transition ease-in-out duration-200" x-transition:enter-start="opacity-0 translate-y-4" x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in-out duration-200" x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4" x-cloak>
+                                                                    <form method="post" action="./handler/subadmin_handler.php" class="bg-white rounded shadow-lg overflow-auto max-w-lg w-full max-h-full" @click.outside="modalOpen = false" @keydown.escape.window="modalOpen = false">
+                                                                        <div class="px-5 py-3 border-b border-gray-200">
+                                                                            <div class="flex justify-between items-center">
+                                                                                <div class="font-semibold text-gray-800">Edit Sub Admin</div>
+                                                                                <button class="text-gray-400 hover:text-gray-500" type="button" @click="modalOpen = false">
+                                                                                    <div class="sr-only">Close</div><svg class="w-4 h-4 fill-current">
+                                                                                        <path d="M7.95 6.536l4.242-4.243a1 1 0 111.415 1.414L9.364 7.95l4.243 4.242a1 1 0 11-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z" /></svg>
+                                                                                </button>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="px-5 py-4">
+                                                                            <div class="space-y-3">
+                                                                                <div>
+                                                                                    <label class="block text-sm font-medium mb-1" for="name">Name <span class="text-red-500">*</span></label> 
+                                                                                    <input name="name" id="name" value="<?= $name; ?>" class="form-input w-full px-2 py-2" required />
+                                                                                </div>
+
+                                                                                <div>
+                                                                                    <label class="block text-sm font-medium mb-1" for="email">Email <span class="text-red-500">*</span></label> 
+                                                                                    <input name="email" id="email" value="<?= $email ?>" class="form-input w-full px-2 py-2" type="email" required />
+                                                                                </div>
+
+                                                                                <div>
+                                                                                    <label class="block text-sm font-medium mb-1" for="feedback">Services <span class="text-red-500">*</span></label> 
+                                                                                    <div class="relative flex flex-col">
+                                                                                        <?php if(count($service->getAllServices())): ?>
+                                                                                            <?php foreach($service->getAllServices() as $service_item):?>
+                                                                                                <div class="m-3">
+                                                                                                    <?php if($service->getService($admin_id) == $service_item['service_id']): ?>
+                                                                                                        <label class="flex items-center">
+                                                                                                            <input type="radio" checked value="<?= $service_item['service_id']; ?>" name="service" class="form-radio"> 
+                                                                                                            <span class="text-sm ml-2"><?= $service_item['service']; ?></span>
+                                                                                                        </label>
+                                                                                                    <?php else: ?>
+                                                                                                        <label class="flex items-center">
+                                                                                                            <input type="radio" checked value="<?= $service_item['service_id']; ?>" name="service" class="form-radio"> 
+                                                                                                            <span class="text-sm ml-2"><?= $service_item['service']; ?></span>
+                                                                                                        </label>
+                                                                                                    <?php endif; ?>
+                                                                                                </div>
+                                                                                            <?php endforeach  ?>
+                                                                                        <?php endif; ?>
+                                                                                    </div>
+                                                                                </div>
+
+                                                                                <div>
+                                                                                    <div class="flex align-center">
+                                                                                        <label class="block text-sm font-medium mb-1" for="feedback">Countries <span class="text-red-500">*</span></label> 
+                                                                                        <div class="relative ml-4" x-data="{ open: false }" @mouseenter="open = true" @mouseleave="open = false">
+                                                                                            <button class="block" aria-haspopup="true" :aria-expanded="open" @focus="open = true" @focusout="open = false" @click.prevent="" aria-expanded="false">
+                                                                                                <svg class="w-4 h-4 fill-current text-gray-400" viewBox="0 0 16 16">
+                                                                                                    <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 12c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1zm1-3H7V4h2v5z"></path>
+                                                                                                </svg>
+                                                                                            </button>
+                                                                                            <div class="z-10 absolute bottom-full left-1/2 transform -translate-x-1/2">
+                                                                                                <div class="min-w-56 bg-gray-800 p-2 rounded overflow-hidden mb-2" x-show="open" x-transition:enter="transition ease-out duration-200 transform" x-transition:enter-start="opacity-0 translate-y-2" x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-out duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" style="display: none;">
+                                                                                                    <div class="text-xs text-gray-200">Hold Ctrl [or Command on Mac] then select the countries</div>
+                                                                                                </div>
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    </div>
+                                                                                    <div class="relative flex flex-col">
+                                                                                        <?php if(count($country_fee)): ?>
+                                                                                            <select class="form-control" name="country[]" multiple="multiple">
+                                                                                                <?php foreach($country_fee as $country => $value):?>
+                                                                                                    <?php if(in_array($country, json_decode($admin->getSubAdmin($admin_id)['countries'], true))): ?>
+                                                                                                        <div class="m-3">
+                                                                                                            <option value="<?= $country; ?>" selected>
+                                                                                                                <?= $country; ?>
+                                                                                                            </option>
+                                                                                                        </div>
+                                                                                                    <?php else: ?>
+                                                                                                        <div class="m-3">
+                                                                                                            <option value="<?= $country; ?>">
+                                                                                                                <?= $country; ?>
+                                                                                                            </option>
+                                                                                                        </div>
+                                                                                                    <?php endif;  ?>
+                                                                                                <?php endforeach  ?>
+                                                                                            </select>
+                                                                                        <?php endif; ?>
+                                                                                        
+                                                                                    </div>
+                                                                                </div>
+
+                                                                            </div>
+                                                                        </div>
+
+                                                                        <div class="px-5 py-4 border-t border-gray-200">
+                                                                            <div class="flex flex-wrap justify-end space-x-2"><button class="btn-sm border-gray-200 hover:border-gray-300 text-gray-600" type="button" @click="modalOpen = false">Cancel</button> 
+                                                                            <button type="submit" name="addAdmin" class="btn-sm bg-indigo-500 hover:bg-indigo-600 text-white">Create</button></div>
+                                                                        </div>
+                                                                    </form>
+                                                                </div>
+                                                            </div>
                                                             <form action="./handler/subadmin_handler.php" method="post">
                                                                 <input type="hidden" name="admin" value="<?= $admin_id; ?>">
                                                                 <button name="deleteAdmin" value="<?= $admin_id; ?>" class="btn text-red-600 text-xs bg-red-100 btn-sm ml-2">Delete</button>
